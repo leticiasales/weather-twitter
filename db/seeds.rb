383117306551ecad5108d48db2486aa4588a4243
leticia sales <letisaless@gmail.com>
2021-01-09 09:28:12 -0300

    modify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,8 @@ require 'json'
 
 file = File.read("city.list.json")
 json = JSON.parse(file)
-cities = []
 
 json.each do |city|
   puts city["name"]
-  cities.push({ name: city["name"], external_id: city["id"] })
+  City.create({ name: city["name"], external_id: city["id"] })
 end
-
-City.create(cities)
