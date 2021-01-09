82ebfb91107ec3de9f6f9d84a888c11bb9bbe1c3
leticia <letisaless@gmail.com>
2021-01-09 14:59:15 -0300

    modify seeds to only us and br

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,14 @@ file = File.read(ENV.fetch("SEED_FILE") { "city.list.json" })
 json = JSON.parse(file)
 
 json.each do |city|
-  puts city["name"]
-  
-  City.create({
-    name: city["name"],
-    state: city["state"],
-    country: city["country"],
-    external_id: city["id"]
-  })
+  if ["US", "BR"].include? city["country"]
+    puts city["name"]
+    
+    City.create({
+      name: city["name"],
+      state: city["state"],
+      country: city["country"],
+      external_id: city["id"]
+    })
+  end
 end
