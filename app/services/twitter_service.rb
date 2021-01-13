5c99317f37ddf138aeb5827a2695745e04dd30ae
leticia <letisaless@gmail.com>
2021-01-13 19:06:18 -0300

    create twitter service and add environment variables

---
+++ app/services/twitter_service.rb
@@ -0,0 +1,14 @@
+class TwitterService
+  def initialize()
+    @twitter = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV["TWITTER_API_KEY"]
+      config.consumer_secret = ENV["TWITTER_API_SECRET"]
+      config.access_token = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+    end
+  end
+
+  def update(tweet)
+    @twitter.update(tweet)
+  end
+end
