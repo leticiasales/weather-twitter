085529482b5a15b66ed3c6ac21dbc6d0cf921bd8
leticia <letisaless@gmail.com>
2021-01-13 15:15:47 -0300

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  scope :api do
+    resources :cities, only: :index
+    post :forecast, to: 'cities#forecast'
+  end
 end
